73505cfa4792061252a75a653e59b476e6e8c698
Lishuo Pan <pls19961219@gmail.com>
2024-12-16 15:08:54 -0500

    update the filter node and luanch file

--- src/fovmpc_estimator/src/filter_node.cpp
+++ src/fovmpc_estimator/src/filter_node.cpp
@@ -45,9 +45,9 @@ class Node
 
 
             // ---------- Subs and Pubs -------------------
-            odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("uav"+std::to_string(ROBOT_ID)+"/mavros/local_position/odom", 1, std::bind(&Node::odom_callback, this, std::placeholders::_1));
-            target_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("uav"+std::to_string(ROBOT_ID)+"/detection"+std::to_string(TARGET_ID), 1, std::bind(&Node::target_callback, this, std::placeholders::_1));
-            est_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/uav"+std::to_string(ROBOT_ID)+"/target_"+std::to_string(TARGET_ID)+"/estimate", 10);
+            odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("mavros/local_position/odom", 1, std::bind(&Node::odom_callback, this, std::placeholders::_1));
+            target_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("detection"+std::to_string(TARGET_ID), 1, std::bind(&Node::target_callback, this, std::placeholders::_1));
+            est_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("target_"+std::to_string(TARGET_ID)+"/estimate", 10);
             timer_ = nh_.createTimer(ros::Duration(1/rate), std::bind(&Node::timer_callback, this));
 
             // Init params
