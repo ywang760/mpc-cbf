119fbfd7521a50a700449432fe858c9bfdb971ba
Lishuo Pan <pls19961219@gmail.com>
2025-01-08 14:01:28 -0500

    update the Animation script

--- experiments/shscripts/AnimationScript.sh
+++ experiments/shscripts/AnimationScript.sh
@@ -6,10 +6,23 @@ max_r=10
 min_fov=120
 max_fov=160
 fov_step=20
+min_slack_decay=0.2
+max_slack_decay=0.3
+slack_decay_step=0.1
+default_slack_decay=0.1
 exp_id=0
 for (( num_r = ${min_r}; num_r <= ${max_r}; num_r++ )); do
   for fov in $(seq ${min_fov} ${fov_step} ${max_fov}); do
     echo Animating simulation of ${num_r} robots...
-    python3 Plotter.py --config_filename "../experiments/instances/${instances}_instances/${instances}${num_r}_fov${fov}_config.json" --states_filename "../experiments/instances/results/log${date}/${instances}${num_r}_fov${fov}_States_${exp_id}.json" --output_filename "../experiments/instances/results/log${date}/${instances}${num_r}.mp4"
+    python3 Plotter.py --config_filename "../experiments/instances/${instances}_instances/${instances}${num_r}_fov${fov}_config.json" --states_filename "../experiments/instances/results/log${date}/${instances}${num_r}_fov${fov}_decay${default_slack_decay}_States_${exp_id}.json" --output_filename "../experiments/instances/results/log${date}/${instances}${num_r}_fov${fov}_decay${default_slack_decay}.mp4"
+  done
+done
+
+for (( num_r = ${min_r}; num_r <= ${max_r}; num_r++ )); do
+  for fov in 120 140; do
+    for slack_decay in $(seq ${min_slack_decay} ${slack_decay_step} ${max_slack_decay}); do
+      echo Animating simulation of ${num_r} robots...
+      python3 Plotter.py --config_filename "../experiments/instances/${instances}_instances/${instances}${num_r}_fov${fov}_config.json" --states_filename "../experiments/instances/results/log${date}/${instances}${num_r}_fov${fov}_decay${slack_decay}_States_${exp_id}.json" --output_filename "../experiments/instances/results/log${date}/${instances}${num_r}_fov${fov}_decay${slack_decay}.mp4"
+    done
   done
 done
